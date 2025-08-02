8ab903453e2cb59c2337df1df43bbda579023580
Arjan Broos <arjanbroos@live.nl>
2025-08-02 21:00:08 +0200

    Change player and tether visualization

--- components/follow_cursor.gd
+++ components/follow_cursor.gd
@@ -7,4 +7,8 @@ extends Node2D
 func _process(delta):
 	var pos = follower.global_position
 	var mousePos = get_global_mouse_position()
+	var direction = pos.direction_to(mousePos)
+	var distance = pos.distance_to(mousePos)
 	follower.global_position = pos.move_toward(mousePos, max_speed * delta)
+	if distance > 1.0:
+		follower.rotation = Vector2.UP.angle_to(direction)
