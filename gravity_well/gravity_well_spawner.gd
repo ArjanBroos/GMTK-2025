f8faf897d161460aa4c5ab77a1e32ac71153dad6
Arjan Broos <arjanbroos@live.nl>
2025-08-03 14:52:46 +0200

    Add more gravity wells over time

--- gravity_well/gravity_well_spawner.gd
+++ gravity_well/gravity_well_spawner.gd
@@ -5,12 +5,14 @@ extends Area2D
 @export var inter_spawn_timer: Timer
 @export var gravity_well_scene: PackedScene
 @export var spawn_area: CollisionShape2D
+@export var initial_spawn_delay_in_seconds: float = 20.0
 
 var well: GravityWell
 
 func _ready() -> void:
 	initial_spawn_timer.timeout.connect(_spawn)
 	inter_spawn_timer.timeout.connect(_spawn)
+	initial_spawn_timer.start(initial_spawn_delay_in_seconds)
 
 func _spawn() -> void:
 	if well != null:
